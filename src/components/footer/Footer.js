ebd25bf436eb4aaf41b795faf38d6e5b4758aa07
EgorSavage <119173955+EgorSavage@users.noreply.github.com>
2023-02-26 12:46:33 +0100

    Delete footer links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,12 +12,6 @@ const Footer = () => {
         <footer className="footer">
             <div className="container">
                 <div className="footer__wrapper">
-                    <ul className="social">
-                        <li className="social__item"><a href="#!"><img src={insta} alt="Link"/></a></li>
-                        <li className="social__item"><a href="#!"><img src={twitter} alt="Link"/></a></li>
-                        <li className="social__item"><a href="#!"><img src={gitHub} alt="Link"/></a></li>
-                        <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link"/></a></li>
-                    </ul>
                     <div className="copyright">
                         <p>© 2023 Yehor Rieznik</p>
                     </div>
