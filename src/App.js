07fe48541e5fd3c1793efe9b4585c1d2487f09a3
EgorSavage <119173955+EgorSavage@users.noreply.github.com>
2023-05-31 10:27:26 +0200

    Изменен на Хеш Роутинг

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./styles/main.css"
 
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { HashRouter as Router, Routes, Route} from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
